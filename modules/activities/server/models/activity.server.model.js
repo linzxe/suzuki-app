2aa2fb669bd52050447e75fc9e86073893c51680
John Mergy <johnmergy@Johns-MacBook-Air.local>
2017-02-16 08:57:39 -0500

    routine commit

---
+++ modules/activities/server/models/activity.server.model.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * Activity Schema
+ */
+var ActivitySchema = new Schema({
+  name: {
+    type: String,
+    default: '',
+    required: 'Please fill Activity name',
+    trim: true
+  },
+  description: {
+    type: String,
+    default: '',
+    required: 'Please add Description',
+    trim: true
+  },
+  teacher: {
+    type: String,
+    default: '',
+    required: 'Please add Coordinator',
+    trim: true
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  }
+});
+
+mongoose.model('Activity', ActivitySchema);
---
+++ modules/teachers/client/config/teachers.client.config.js
@@ -0,0 +1,32 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('teachers')
+    .run(menuConfig);
+
+  menuConfig.$inject = ['Menus'];
+
+  function menuConfig(Menus) {
+    // Set top bar menu items
+    Menus.addMenuItem('topbar', {
+      title: 'Teachers',
+      state: 'teachers',
+      type: 'dropdown',
+      roles: ['*']
+    });
+
+    // Add the dropdown list item
+    Menus.addSubMenuItem('topbar', 'teachers', {
+      title: 'List Teachers',
+      state: 'teachers.list'
+    });
+
+    // Add the dropdown create item
+    Menus.addSubMenuItem('topbar', 'teachers', {
+      title: 'Create Teacher',
+      state: 'teachers.create',
+      roles: ['user']
+    });
+  }
+}());
